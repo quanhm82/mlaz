b4fbccb55b328428910e64f9cb59db0c7165e143
Quan Hoang <quanhm82@gmail.com>
2017-01-18 00:42:38 +0700

    First nitialize

---
+++ mlaz/src/main/java/com/mlaz/api/MlazApplication.java
@@ -0,0 +1,12 @@
+package com.mlaz.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlazApplication.class, args);
+	}
+}
