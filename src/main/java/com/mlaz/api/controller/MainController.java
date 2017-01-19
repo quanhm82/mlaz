450a035c305439d4e1abb9fab8119b91d7220513
Quan Hoang <quanhm82@gmail.com>
2017-01-19 23:24:10 +0700

    Add: MainController and MlazService

---
+++ src/main/java/com/mlaz/api/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.mlaz.api.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author quanhoang
+ * Main Controller for MLAZ API
+ */
+@RestController
+public class MainController {
+
+}
---
+++ src/main/java/com/mlaz/api/model/MlazService.java
@@ -0,0 +1,9 @@
+package com.mlaz.api.model;
+
+/**
+ * @author quanhoang
+ * Model data for MLAZ Service
+ */
+public class MlazService {
+
+}
