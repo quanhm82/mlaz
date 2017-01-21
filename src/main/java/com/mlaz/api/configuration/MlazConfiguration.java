47bb494298d3a61631a38d9109abc0fdc2563b7b
Quan Hoang <quanhm82@gmail.com>
2017-01-21 22:04:40 +0700

    Separate DB configuration into DynamoDbConfiguration file

--- src/main/java/com/mlaz/api/configuration/MlazConfiguration.java
+++ src/main/java/com/mlaz/api/configuration/MlazConfiguration.java
@@ -1,40 +1,12 @@
 package com.mlaz.api.configuration;
 
-import org.apache.commons.lang.StringUtils;
-import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableDynamoDBRepositories(basePackages = "com.mlaz.api.repositories")
+@EnableWebMvc
+@Import(DynamoDbConfiguration.class)
 public class MlazConfiguration {
-	@Value("${amazon.dynamodb.endpoint}")
-    private String amazonDynamoDBEndpoint;
-
-    @Value("${amazon.aws.accesskey}")
-    private String amazonAWSAccessKey;
-
-    @Value("${amazon.aws.secretkey}")
-    private String amazonAWSSecretKey;
-
-    @Bean
-    public AmazonDynamoDB amazonDynamoDB() {
-        AmazonDynamoDB amazonDynamoDB = new AmazonDynamoDBClient(
-                amazonAWSCredentials());
-        if (StringUtils.isNotEmpty(amazonDynamoDBEndpoint)) {
-            amazonDynamoDB.setEndpoint(amazonDynamoDBEndpoint);
-        }
-        return amazonDynamoDB;
-    }
-
-    @Bean
-    public AWSCredentials amazonAWSCredentials() {
-        return new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
-    }
+	
 }
