3d8d1f94770c21cd20f99a87a8b56670e1f8e928
tuan.nguyen <tuan.nguyen@jayeson.com.sg>
2017-02-05 02:23:14 +0800

    add devtools, bypass security if dev mode

--- src/main/java/com/mlaz/api/controller/MainController.java
+++ src/main/java/com/mlaz/api/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.mlaz.api.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,4 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
+    @RequestMapping(value = "/")
+    public String index() {
+        return "Hello World";
+    }
 }
