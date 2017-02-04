d16c0983e472b6ed76cd6410e05ac5160a513980
tuan.nguyen <tuan.nguyen@jayeson.com.sg>
2017-02-05 03:30:45 +0800

    implement service rest api (list, add and delete)

---
+++ src/main/java/com/mlaz/api/controller/ServiceController.java
@@ -0,0 +1,34 @@
+package com.mlaz.api.controller;
+
+import com.mlaz.api.model.MlazService;
+import com.mlaz.api.repositories.MlazServiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by jimmy on 5/2/17.
+ */
+@RestController
+public class ServiceController {
+
+    @Autowired
+    private MlazServiceRepository mlazServiceRepository;
+
+    @RequestMapping(value = "services", method = RequestMethod.GET)
+    public Iterable<MlazService> list() {
+        return mlazServiceRepository.findAll();
+    }
+
+    @RequestMapping(value = "services/add", method = RequestMethod.POST)
+    public MlazService add(@RequestParam("name") String name, @RequestParam("unit") String unit) {
+        MlazService mlazService =  new MlazService(name, unit);
+
+        return mlazServiceRepository.save(mlazService);
+    }
+
+    @RequestMapping(value = "services/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") String id, Model model) {
+        mlazServiceRepository.delete(id);
+    }
+}
